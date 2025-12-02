60cde1180fc93da6bb56450f491e8fea7a0c3f6d
Chessenok <110049758+Chessenok@users.noreply.github.com>
2025-12-02 12:08:31 +0200

    Modified README

--- SmesnoiM_Tema9/SlotsWindow.cs
+++ SmesnoiM_Tema9/SlotsWindow.cs
@@ -34,41 +34,7 @@ public class SlotsWindow : GameWindow
     {
         VSync = VSyncMode.On;
     }
-
-    public class Texture
-    {
-        public int Handle;
-
-        public Texture(string path)
-        {
-            Handle = GL.GenTexture();
-            GL.BindTexture(TextureTarget.Texture2D, Handle);
-
-            StbImage.stbi_set_flip_vertically_on_load(1);
-
-            using (Stream stream = File.OpenRead(path))
-            {
-                ImageResult image = ImageResult.FromStream(stream, ColorComponents.RedGreenBlueAlpha);
-
-                GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba,
-                    image.Width, image.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, image.Data);
-            }
-
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.ClampToEdge);
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.ClampToEdge);
-
-            GL.BindTexture(TextureTarget.Texture2D, 0);
-        }
-
-        public void Use()
-        {
-            GL.ActiveTexture(TextureUnit.Texture0);
-            GL.BindTexture(TextureTarget.Texture2D, Handle);
-        }
-    }
-
+    
 
     protected override void OnLoad(EventArgs e)
     {
